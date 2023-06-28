225fb982b95c3c3d983e76d6034f8bab6bf7e37d
cliff <clifflee1022@gmail.com>
2023-06-28 21:59:53 +0800

    feat: home/desktop nav mobile ui

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,17 +6,19 @@ import { useState, useEffect } from "react"
 import { signIn, signOut, useSession, getProviders } from "next-auth/react"
 
 const Nav = () => {
-  const isUserLoggedIn = false
+  const isUserLoggedIn = true
 
   const [providers, setProviders] = useState(null)
+  const [toggleDropdown, setToggleDropdown] = useState(false)
 
   useEffect(() => {
     // (async () => {
     //   const res = await getProviders()
     //   setProviders(res)
     // })()
-    const setUpProviders = async() => {
+    const setUpProviders = async () => {
       const response = await getProviders()
+      console.log('---setUpProviders:', response)
       setProviders(response)
     }
 
@@ -59,17 +61,77 @@ const Nav = () => {
           </div>
         ) : (
           <>
-            {providers && Object.values(providers).map((provider) => (
-              // 此处仅有一个 Provider google
-              <button
-                type="button"
-                key={provider.name}
-                onClick={() => signIn(provider.id)}
-                className="black_btn"
-              >
-                Sign In
-              </button>
-            ))}
+            {providers &&
+              Object.values(providers).map((provider) => (
+                // 此处仅有一个 Provider google
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+
+      {/* Mobile Navigation */}
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image
+              src="/assets/images/logo.svg"
+              width={37}
+              height={37}
+              className="rounded-full"
+              alt="profile"
+              onClick={() => setToggleDropdown((prev) => !prev)}
+            />
+
+            {toggleDropdown && (
+              <div className="dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button type="button"
+                  onClick={() => {
+                    setToggleDropdown(false)
+                    signOut()
+                  }}
+                  className="mt-5 w-full black_btn"
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                // 此处仅有一个 Provider google
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
           </>
         )}
       </div>
