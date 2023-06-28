8f12fc8455d6097775f3f4aed1cd38536e99ff76
cliff <clifflee1022@gmail.com>
2023-06-28 22:04:02 +0800

    feat: home add provider component

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,24 @@
 import "@styles/globals.css"
-import Nav from '@components/Nav'
-import Provider from '@components/Provider'
+import Nav from "@components/Nav"
+import Provider from "@components/Provider"
 
 export const metadata = {
   title: "Promptopia",
-  discription:  "Discover and Share AI prompts"
+  discription: "Discover and Share AI prompts",
 }
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient"/>
-        </div>
-        <main className="app">
-          <Nav/>
-          {children}
-        </main>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   )
