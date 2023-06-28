e4afd43322104cb47152d67e4478ac0b44c3859f
cliff <clifflee1022@gmail.com>
2023-06-28 21:47:35 +0800

    feat: home/desktop navbar no login ui & add providers logic

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,7 +7,22 @@ import { signIn, signOut, useSession, getProviders } from "next-auth/react"
 
 const Nav = () => {
   const isUserLoggedIn = false
-  
+
+  const [providers, setProviders] = useState(null)
+
+  useEffect(() => {
+    // (async () => {
+    //   const res = await getProviders()
+    //   setProviders(res)
+    // })()
+    const setUpProviders = async() => {
+      const response = await getProviders()
+      setProviders(response)
+    }
+
+    setUpProviders()
+  }, [])
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -44,7 +59,17 @@ const Nav = () => {
           </div>
         ) : (
           <>
-
+            {providers && Object.values(providers).map((provider) => (
+              // 此处仅有一个 Provider google
+              <button
+                type="button"
+                key={provider.name}
+                onClick={() => signIn(provider.id)}
+                className="black_btn"
+              >
+                Sign In
+              </button>
+            ))}
           </>
         )}
       </div>
