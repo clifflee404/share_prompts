d50de56e955e0772be9ab65de5b8548da8fbd8e7
cliff <clifflee1022@gmail.com>
2023-06-29 16:26:02 +0800

    feat: [feedpage] feed page ui and api/prompt ok

---
+++ app/api/prompt/route.js
@@ -0,0 +1,15 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const GET = async(request) => {
+  try {
+    await connectToDB()
+
+    // todo 研究 populate 什么意思?
+    const prompts = await Prompt.find({}).populate('creator')
+
+    return new Response(JSON.stringify(prompts), { status: 200 })
+  } catch (error) {
+    return new Response("Failed to fetch all prompts", { status: 500 })
+  }
+}
